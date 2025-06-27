4109fce03e1bbe27f6d627414deef7311ff168b1
Grebnik89 <116430298+art-just-code@users.noreply.github.com>
2025-06-27 19:31:41 +0300

    terraform apply complete

--- clickhouse/main.tf
+++ clickhouse/main.tf
@@ -1,10 +1,20 @@
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "docker_image" "this" {
   name = var.image
 }
 
 resource "docker_container" "this" {
   name  = var.container_name
-  image = docker_image.this.latest
+  image = docker_image.this.image_id
 
   ports {
     internal = 9000
@@ -17,7 +27,7 @@ resource "docker_container" "this" {
   }
 
   volumes {
-    host_path      = "${path.module}/data"
+    host_path      = abspath("${path.module}/data")
     container_path = "/var/lib/clickhouse"
   }
 
