af7966791914bc2c46b6ed1fb922ed200649dd13
Grebnik89 <116430298+art-just-code@users.noreply.github.com>
2025-07-03 19:18:30 +0300

    complete task, add screenshots

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -19,6 +19,8 @@ def write_clickhouse(data):
     client = Client(
         host=os.getenv("CLICKHOUSE_HOST", "localhost"),
         port=int(os.getenv("CLICKHOUSE_PORT", 9000)),
+        user=os.getenv("CLICKHOUSE_USER", "default"),
+        password=os.getenv("CLICKHOUSE_PASSWORD", "default"),
     )
     client.execute(
         """
